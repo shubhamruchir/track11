113f379a19ae56f293cb6d7d25d390159e1bda89
shubhamruchir <shubhamruchir@gmail.com>
2026-03-22 01:40:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-
+console.log("CLIENT_ID:", process.env.CLIENT_ID);
 // ✅ ENV VARIABLES
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
